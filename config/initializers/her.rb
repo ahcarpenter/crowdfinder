703064260d4f6f44b1a01e19f6b812fe330c6ff5
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2016-06-09 15:38:51 -0400

    revise key

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,7 +1,7 @@
 Her::API.setup url: 'https://www.eventbriteapi.com' do |c|
   #Request
   c.use FaradayMiddleware::Caching, Rails.cache
-  c.use FaradayMiddleware::OAuth2, ENV['eventbrite_access_token'], {param_name: 'token'}
+  c.use FaradayMiddleware::OAuth2, ENV['EVENTBRITE_ACCESS_TOKEN'], {param_name: 'token'}
   c.use Eventbrite::TokenAuthentication
 
   # Response
