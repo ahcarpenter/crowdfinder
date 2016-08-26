d4994f949bdce47bb7710f9b11ee0eab5f5c5054
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2016-08-25 21:41:03 -0400

    revise caching scheme

--- app/jobs/prefetch_event_data_job.rb
+++ app/jobs/prefetch_event_data_job.rb
@@ -3,11 +3,13 @@ class PrefetchEventDataJob < ApplicationJob
 
   def perform(*args)
     (1..19).each do |page|
+      Rails.cache.delete("/events/search?location.address=Los+Angeles%2C+CA&page=#{page}")
       @events = Eventbrite::Event.by_address('Los Angeles, CA')
                   .page(page).fetch
       print "Event data for page #{page} fetched!\n"
 
       @events.each do |event|
+        Rails.cache.delete("/organizers/#{event.organizer.id}")
         event.organizer.fetch
         print "Organizer data for event #{event.id} fetched!\n"
       end
