c4a2c0ec271ed3e5fe942c935ae5f7b1552334b6
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2016-08-25 21:53:25 -0400

    revise number of pages cached

--- app/jobs/prefetch_event_data_job.rb
+++ app/jobs/prefetch_event_data_job.rb
@@ -2,7 +2,7 @@ class PrefetchEventDataJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    (1..19).each do |page|
+    (1..5).each do |page|
       Rails.cache.delete("/events/search?location.address=Los+Angeles%2C+CA&page=#{page}")
       @events = Eventbrite::Event.by_address('Los Angeles, CA')
                   .page(page).fetch
